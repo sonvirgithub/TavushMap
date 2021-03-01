edcbf278565a2ecdc7906db35537420e3fd01aa1
linamkrtumyan <mkrtumyanlina@gmail.com>
2021-03-01 11:37:21 +0400

    Lina organization page

--- tavush_maps/src/Routes.js
+++ tavush_maps/src/Routes.js
@@ -1,36 +1,45 @@
-import React from 'react';
-import Login from './components/Login/Login';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import RouteWithLayout from './router/RouteWithLayout'
-import LoginLayout from './router/MainLayout'
-import MainLayout from './router/LoginLayout'
-import HomePage from './components/HomePage/HomePage'
+import React from "react";
+import Login from "./components/Login/Login";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import RouteWithLayout from "./router/RouteWithLayout";
+import LoginLayout from "./router/MainLayout";
+import MainLayout from "./router/LoginLayout";
+import HomePage from "./components/HomePage/HomePage";
+import Organization from "./components/Organization/Organization";
+import OrganizationsPage from "./pages/OrganizationsPage";
 
 function Routes() {
+  return (
+    <>
+      <Switch>
+        <RouteWithLayout
+          component={Login}
+          exact
+          layout={LoginLayout}
+          path="/login"
+        />
 
-    return (
-        <>
-
-            <Switch>
-
-                <RouteWithLayout
-                    component={Login}
-                    exact
-                    layout={LoginLayout}
-                    path="/login"
-                />
-
-                <RouteWithLayout
-                    component={HomePage}
-                    exact
-                    layout={MainLayout}
-                    path="/home"
-                />
-                 <Redirect to='/login' /> 
-
-            </Switch>
-        </>
-    )
+        <RouteWithLayout
+          component={HomePage}
+          exact
+          layout={MainLayout}
+          path="/home"
+        />
+        <RouteWithLayout
+          component={OrganizationsPage}
+          exact
+          layout={MainLayout}
+          path="/organization"
+        />
+        <Redirect to="/login" />
+      </Switch>
+    </>
+  );
 }
 
-export default Routes
+export default Routes;
