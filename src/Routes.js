cbc52f9cf4c9f23e2cd59bdda828dc0bf4bfe769
linamkrtumyan <mkrtumyanlina@gmail.com>
2021-03-01 14:40:18 +0400

    no message

--- src/Routes.js
+++ src/Routes.js
@@ -1,43 +1,51 @@
-import React from 'react';
-import Login from './components/Login/Login';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import RouteWithLayout from './router/RouteWithLayout'
-import LoginLayout from './router/LoginLayout'
-import MainLayout from './router/MainLayout'
-import HomePage from './components/HomePage/HomePage'
-import OrganizationsPage from "./pages/OrganizationsPage"
+import React from "react";
+import Login from "./components/Login/Login";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import RouteWithLayout from "./router/RouteWithLayout";
+import LoginLayout from "./router/LoginLayout";
+import MainLayout from "./router/MainLayout";
+import HomePage from "./components/HomePage/HomePage";
+import OrganizationsPage from "./pages/OrganizationsPage";
+import CategoriesPage from "./pages/CategoriesPage";
 
 function Routes() {
+  return (
+    <>
+      <Switch>
+        <RouteWithLayout
+          component={Login}
+          exact
+          layout={LoginLayout}
+          path="/login"
+        />
 
-    return (
-        <>
-
-            <Switch>
-
-                <RouteWithLayout
-                    component={Login}
-                    exact
-                    layout={LoginLayout}
-                    path="/login"
-                />
-
-                <RouteWithLayout
-                    component={HomePage}
-                    exact
-                    layout={MainLayout}
-                    path="/home"
-                />
-                <RouteWithLayout
-                    component={OrganizationsPage}
-                    exact
-                    layout={MainLayout}
-                    path="/organization"
-                />
-                 <Redirect to='/login' /> 
-
-            </Switch>
-        </>
-    )
+        <RouteWithLayout
+          component={HomePage}
+          exact
+          layout={MainLayout}
+          path="/home"
+        />
+        <RouteWithLayout
+          component={OrganizationsPage}
+          exact
+          layout={MainLayout}
+          path="/organization"
+        />
+        <RouteWithLayout
+          component={CategoriesPage}
+          exact
+          layout={MainLayout}
+          path="/category"
+        />
+        <Redirect to="/login" />
+      </Switch>
+    </>
+  );
 }
 
-export default Routes
+export default Routes;
